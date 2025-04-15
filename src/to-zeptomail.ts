f4644298ec64971bc8fe288f2f2b63e8bcf9d074
Stephan Troyer <stephantroyer@live.at>
2025-04-15 23:54:19 +0200

    switch to zeptomail rest API

--- src/to-zeptomail.ts
+++ src/to-zeptomail.ts
@@ -1,5 +1,4 @@
 // @ts-ignore
-import { SendMailClient } from 'zeptomail';
 import { EmailOptions } from './index.js';
 import { encodeAttachmentToBase64, parseMail } from './tools.js';
 
@@ -15,17 +14,26 @@ export async function sendEmail(email: React.ReactElement,
     if (!url) throw new Error('No url provided for Zeptomail. You can also set the environment variable `ZEPTOMAIL_URL`.');
     if (!token) throw new Error('No token provided for Zeptomail. You can also set the environment variable `ZEPTOMAIL_TOKEN`.');
 
-    const client = new SendMailClient({ url, token });
-
-    await client.sendMail({
-        from: { address: from.email, name: from.name },
-        to: to.map(({ email, name }) => ({ "email_address": { address: email, name } })),
-        cc: cc?.map(({ email, name }) => ({ "email_address": { address: email, name } })),
-        bcc: bcc?.map(({ email, name }) => ({ "email_address": { address: email, name } })),
-        replyTo: reply_to ? [{ address: reply_to.email, name: reply_to.name }] : undefined,
-        subject: options.subject,
-        textbody: text,
-        htmlbody: html,
-        attachments: options.attachments?.map(attachment => ({"mime_type": attachment.contentType, "name": attachment.filename, "content": encodeAttachmentToBase64(attachment.content)})),
+    const result = await fetch(`https://${url}/v1.1/email`, {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'Authorization': token,
+        },
+        body: JSON.stringify({
+            from: { address: from.email, name: from.name },
+            to: to.map(({ email, name }) => ({ "email_address": { address: email, name } })),
+            cc: cc?.map(({ email, name }) => ({ "email_address": { address: email, name } })),
+            bcc: bcc?.map(({ email, name }) => ({ "email_address": { address: email, name } })),
+            replyTo: reply_to ? [{ address: reply_to.email, name: reply_to.name }] : undefined,
+            subject: options.subject,
+            textbody: text,
+            htmlbody: html,
+            attachments: options.attachments?.map(attachment => ({ "mime_type": attachment.contentType, "name": attachment.filename, "content": encodeAttachmentToBase64(attachment.content) })),
+        }),
     });
+
+    if (result.status >= 200 && result.status < 300) return;
+    throw new Error(`Error sending email: ${result.status} ${await result.text()}`);
 }
